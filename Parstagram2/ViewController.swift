179fa69d5544aa7810563bd77854a27b08e04d2e
Songeun Kim <faborange610@gmail.com>
2020-02-27 01:53:00 -0800

    Initial Commit

---
+++ Parstagram2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram2
+//
+//  Created by 김송은 on 2/27/20.
+//  Copyright © 2020 faborange610@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
